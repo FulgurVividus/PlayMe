3081a640ea69d66adbbf38f2fa1924a3cee12ad8
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-01 22:56:24 +0500

    chore

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,10 @@ import HeaderContent from "./HeaderContent";
 const Header: React.FC = () => {
   return (
     <>
-      <div className="bg-headerColor px-[22px] lg:px-[0]">
+      <section className="bg-headerColor px-[22px] lg:px-[0]">
         <Navigation />
         <HeaderContent />
-      </div>
+      </section>
     </>
   );
 };

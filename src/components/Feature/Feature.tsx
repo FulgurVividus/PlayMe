6552c7422cbfca26dc797b6efb7771faca1d0497
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-01 22:54:07 +0500

    feat: Feature component

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,7 +1,71 @@
 import React from "react";
 
+import arrow from "../../assets/images/feature-images/arrow.png";
+import featureImage1 from "../../assets/images/feature-images/featureImage1.png";
+import featureImage2 from "../../assets/images/feature-images/featureImage2.png";
+import featureImage3 from "../../assets/images/feature-images/featureImage3.png";
+
+const cards: {
+  id: number;
+  img: string;
+  title: string;
+  text: string;
+  link: string;
+}[] = [
+  {
+    id: 1,
+    img: featureImage1,
+    title: "Администраторы и директоры",
+    text: "Управление регистрацией, выставлением счетов, оплата труда, связь и бизнес из приложение Playme.",
+    link: "Оставить заявку",
+  },
+  {
+    id: 2,
+    img: featureImage2,
+    title: "Преподаватели и персонал",
+    text: "Легко управляйте повседневными задачами, от регистрация детей и мероприятия, до общение с родителями.",
+    link: "Оставить заявку",
+  },
+  {
+    id: 3,
+    img: featureImage3,
+    title: "Родители или опекуны",
+    text: "Оставайтесь на связи с вашим ребенком и легко настроивать онлайн-платежи.",
+    link: "Оставить заявку",
+  },
+];
+
 const Feature: React.FC = () => {
-  return <div>Feature</div>;
+  return (
+    <>
+      <section className="bg-featureColor mt-[80px] rounded-[40px] custom-container">
+        <p className="text-white text-[28px] leading-[40px] font-bold text-center pt-8 lg:pt-[46px]">
+          Разработано с учетом ваших потребностей
+        </p>
+
+        {/* Cards */}
+        <ul className="flex flex-col lg:flex-row mt-8 lg:mt-[17px] gap-[15px] px-[15px] lg:px-[120px] pb-12">
+          {cards.map((card) => (
+            <li
+              key={card.id}
+              className="bg-white h-[336px] rounded-[12px] p-[25px] lg:p-[24px] flex flex-col gap-4 items-center w-full"
+            >
+              <img loading="lazy" src={card.img} alt="image" />
+              <p className="font-black text-lg text-center">{card.title}</p>
+              <p className="text-center font-inter text-[#5c5e6a] flex-grow">
+                {card.text}
+              </p>
+
+              <a href="#" className="flex items-center gap-3 text-blue-500">
+                <span>{card.link}</span>
+                <img loading="lazy" src={arrow} alt="arrow" />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 };
 
 export default Feature;

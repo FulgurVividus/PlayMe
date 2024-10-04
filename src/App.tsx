3cfa2653066a0b72cd1ca3884725e75517375787
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-04 22:32:18 +0500

    perf: performance optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Feature from "./components/Feature/Feature";
-import MainProductDescription from "./components/MainProductDescription/MainProductDescription";
-import Stats from "./components/Stats/Stats";
-import Partnership from "./components/Partnership/Partnership";
-import FeatureList from "./components/FeatureList/FeatureList";
-import CallToAction from "./components/CallToAction/CallToAction";
-import Footer from "./components/Footer/Footer";
+
+const Header = React.lazy(() => import("./components/Header/Header"));
+const Feature = React.lazy(() => import("./components/Feature/Feature"));
+const MainProductDescription = React.lazy(
+  () => import("./components/MainProductDescription/MainProductDescription")
+);
+const Stats = React.lazy(() => import("./components/Stats/Stats"));
+const Partnership = React.lazy(
+  () => import("./components/Partnership/Partnership")
+);
+const FeatureList = React.lazy(
+  () => import("./components/FeatureList/FeatureList")
+);
+const CallToAction = React.lazy(
+  () => import("./components/CallToAction/CallToAction")
+);
+const Footer = React.lazy(() => import("./components/Footer/Footer"));
 
 const App: React.FC = () => {
   return (
     <>
-      <div>
-        <Header />
-        <Feature />
-        <MainProductDescription />
-        <Stats />
-        <Partnership />
-        <FeatureList />
-        <CallToAction />
-        <Footer />
-      </div>
+      <Header />
+      <Feature />
+      <MainProductDescription />
+      <Stats />
+      <Partnership />
+      <FeatureList />
+      <CallToAction />
+      <Footer />
     </>
   );
 };

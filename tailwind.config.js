7a6102d224846bbc74a5ded41db57ead9c378f1e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-09 23:54:27 +0500

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         headerColor: "#057FE4",
         featureColor: "#057FE4",
         callToActionColor: "#F7F9FB",
-        footerColor: "#1E2549",
+        footerColor: "#F7F9FB",
       },
     },
   },

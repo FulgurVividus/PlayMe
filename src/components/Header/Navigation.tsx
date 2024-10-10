294524049fa6568d3d4c2dc821fdbd21d1d475f3
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-10 14:41:28 +0500

    feat: quiz link

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -14,15 +14,27 @@ const Navigation: React.FC = () => {
           className="w-[200px] h-[60px] select-none"
         />
 
-        {/* TODO: Make the button functional */}
-        <button
-          title="button"
-          className="py-[8px] px-[18px] rounded-md bg-white hidden md:block"
-        >
-          <span className="leading-[28px] text-black font-black font-inter">
-            Демо версия
-          </span>
-        </button>
+        <div className="flex items-center gap-x-2">
+          <a
+            href="https://forms.gle/rNGdhhHA1Re5ya4C9"
+            target="_blank"
+            className="py-[8px] px-[18px] rounded-md bg-white hover:opacity-90 duration-75"
+          >
+            <span className="leading-[28px] text-black font-black font-inter">
+              Пройти опрос
+            </span>
+          </a>
+
+          {/* TODO: Make the button functional */}
+          <button
+            title="button"
+            className="py-[8px] px-[18px] rounded-md bg-white hidden md:block hover:opacity-90 duration-75"
+          >
+            <span className="leading-[28px] text-black font-black font-inter">
+              Демо версия
+            </span>
+          </button>
+        </div>
       </nav>
     </>
   );
